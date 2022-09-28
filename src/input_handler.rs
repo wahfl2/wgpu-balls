837a108de9ada5434c58988d04a41c3efde3e2bc
wahfl2 <wahflbeljihn@gmail.com>
2022-09-28 10:31:14 -0500

    Added mouse buttons

--- src/input_handler.rs
+++ src/input_handler.rs
@@ -1,14 +1,15 @@
-use cgmath::Vector2;
 use winit::{event::{KeyboardInput, ElementState}, dpi::PhysicalPosition};
 
 pub struct InputHandler {
     mouse_pos: Option<PhysicalPosition<f64>>,
+    button_states: [bool; 32],
 }
 
 impl InputHandler {
     pub fn new() -> Self {
         Self {
-            mouse_pos: None
+            mouse_pos: None,
+            button_states: [false; 32],
         }
     }
 
@@ -20,7 +21,11 @@ impl InputHandler {
         self.mouse_pos = Some(input.to_owned());
     }
 
-    pub fn handle_mouse_button(&self, button_id: u32, state: ElementState ) {
+    pub fn handle_mouse_button(&mut self, button_id: u32, state: ElementState ) {
+        if button_id >= 32 { panic!("There's no way you have 33 buttons on your mouse wtf") };
+        let pressed = state == ElementState::Pressed;
+        self.button_states[button_id as usize] = pressed;
+
         
     }
 }
