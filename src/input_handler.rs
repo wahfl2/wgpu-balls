6dbab12a69d2bbc07501c9c7c1f5b3c032e49613
wahfl2 <wahflbeljihn@gmail.com>
2022-09-30 13:11:40 -0500

    INPUT BALLS

--- src/input_handler.rs
+++ src/input_handler.rs
@@ -1,4 +1,7 @@
+use cgmath::{Vector2, Quaternion, AbsDiffEq, Vector3};
 use winit::{event::{KeyboardInput, ElementState}, dpi::PhysicalPosition};
+
+use crate::{render_state::RenderState, instance::Instance};
 
 pub struct InputHandler {
     mouse_pos: Option<PhysicalPosition<f64>>,
@@ -21,11 +24,21 @@ impl InputHandler {
         self.mouse_pos = Some(input.to_owned());
     }
 
-    pub fn handle_mouse_button(&mut self, button_id: u32, state: ElementState ) {
+    pub fn handle_mouse_button(&mut self, button_id: u32, state: ElementState, render_state: &mut RenderState ) {
         if button_id >= 32 { panic!("There's no way you have 33 buttons on your mouse wtf") };
         let pressed = state == ElementState::Pressed;
         self.button_states[button_id as usize] = pressed;
 
-        
+        if button_id == 1 && pressed {
+            let pos = self.mouse_pos.unwrap_or_default();
+    
+            let instance_add = Instance {
+                position: Vector2::new(pos.x as f32, pos.y as f32),
+                rotation: Quaternion::from_sv(1.0, Vector3::unit_y()),
+                scale: 100.0,
+            };
+
+            render_state.add_instance(instance_add);
+        }
     }
 }
